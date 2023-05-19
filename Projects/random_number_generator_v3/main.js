e78a190a3bb5cdccf0d641fee792aea54d0dbda9
mate1225 <130367999+mate1225@users.noreply.github.com>
2023-05-19 15:30:40 +0200

    Add files via upload

--- Projects/random_number_generator_v3/main.js
+++ Projects/random_number_generator_v3/main.js
@@ -1,54 +1,58 @@
-let count=0;
+let count = 0;
+let result;
 //button input
-const btn=document.getElementById("btn");
+const btn = document.getElementById("btn");
 //funcions
-function randomNumberGenerator(lowerLimit,upperLimit){
-    return Math.floor(Math.random()*(upperLimit - lowerLimit+1) + lowerLimit);
+function randomNumberGenerator(lowerLimit, upperLimit) {
+  return Math.floor(Math.random() * (upperLimit - lowerLimit + 1) + lowerLimit);
 }
+
+function displayPTag(result) {
+    const displayNumbers = document.querySelector(".pTags");
+  //counting the number of generated number
+  count = count + 1;
+  //creates a <p> element
+  const elementNode = document.createElement("p");
+  const textNode = document.createTextNode(
+    "A(z) " + count + ". szám: " + result
+  );
+  //puts the textNode insade the <p> element
+  elementNode.appendChild(textNode);
+  //display the resoult to the users
+  displayNumbers.appendChild(elementNode);
+}
+
 //evetns
-btn.addEventListener("click",()=>{
-    let resoult;
+btn.addEventListener("click", () => {
+  
 
-    function displayPTag(resoult){
-        //counting the number of generated number
-        count=count+1;
-        //creates a <p> element
-        const elementNode=document.createElement('p');
-        const textNode=document.createTextNode("A(z) "+count+". szám: "+result);
-        //puts the textNode insade the <p> element
-        elementNode.appendChild(textNode);
-        //display the resoult to the users
-        displayNumbers.appendChild(elementNode);
-    }
-
-
-
-    //inputs
-    let lowerLimit=document.getElementById("lowerLimit").value;
-    let upperLimit=document.getElementById("upperLimit").value;
-    const displayNumbers=document.querySelector(".pTags");
-    //converting string to number
-    lowerLimit = parseInt(lowerLimit);
-    upperLimit = parseInt(upperLimit);
-    //puting the input into the random number generator
-    if(lowerLimit>upperLimit){
+  //inputs
+  let lowerLimit = document.getElementById("lowerLimit").value;
+  let upperLimit = document.getElementById("upperLimit").value;
+  
+  //converting string to number
+  lowerLimit = parseInt(lowerLimit);
+  upperLimit = parseInt(upperLimit);
+  //puting the input into the random number generator
+  if(lowerLimit>upperLimit){
         window.alert("Az alsó határ nem lehet nagyobb mint a felső");
-     }
-    else if(lowerLimit && upperLimit){
-       result=randomNumberGenerator(lowerLimit,upperLimit);
-       displayPTag(result);
     }
-    else  if(lowerLimit){
-        result=randomNumberGenerator(lowerLimit,100);
-        displayPTag(result);
-    }
-    else if(upperLimit){
-        result=randomNumberGenerator(0,upperLimit);
-        displayPTag(result);
-    }
-    else {
-        result=randomNumberGenerator(0,100);
-        displayPTag(result);
-    };
+  else if (lowerLimit && upperLimit) {
+    result = randomNumberGenerator(lowerLimit, upperLimit);
+    displayPTag(result);
+  } else if (lowerLimit) {
+    result = randomNumberGenerator(lowerLimit, 100);
+    displayPTag(result);
+  } else if (upperLimit) {
+    result = randomNumberGenerator(0, upperLimit);
+    displayPTag(result);
+  } else {
+    result = randomNumberGenerator(0, 100);
+    displayPTag(result);
+  }
 });
-
+//refresh the page (and resets the results)
+const resetBtn=document.getElementById("resetBtn");
+resetBtn.addEventListener('click',()=>{
+  location.reload();
+}); 
